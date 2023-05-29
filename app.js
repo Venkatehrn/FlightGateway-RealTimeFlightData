85c49793de4f15eda560c374a357bd149fdaa989
Venkatesh Nadagouda <venkateshnadagouda654@gmail.com>
2023-05-29 09:29:59 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+ const express = require('express')
+const axios = require('axios')
+const config = require('./config.json')
+
+const server = express();
+
+
+
+const getRealTimeData = async(flightcode) =>{
+    https://www.flightradar24.com/data/airports/lcy/routes"}
+
+   var getFilePath = config.serverAPI + 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+server.get('flight data',async (req,res) =>{
+    var myParams = req,params;
+});
+
+server.listen(3000, ()=>{
+    console.log('Flight gateway has started....')
+});
+ {
+    "serverAPI" :
+    "accesskey" :
+}
